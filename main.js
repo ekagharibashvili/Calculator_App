52c6330a80f6d006406260dc18c4b194fa0e50e7
Eka Gharibashvili <ekagaribashvili98@gmail.com>
2021-06-12 18:35:26 +0400

    Update main.js

--- main.js
+++ main.js
@@ -58,7 +58,7 @@ function mathOperation() {
   } else if (lastOperation === "/") {
     result = parseFloat(result) / parseFloat(dis2Num);
   } else if (lastOperation === "%") {
-    result = parseFloat(result) % parseFloat(dis2Num);
+    result = parseFloat(result) * parseFloat(dis2Num) / 100;
   }
 }
 // operation();
